33c84d7ecefc193146acb9832813d62a0542094b
Nick <nick.young@auckland.ac.nz>
2020-02-03 11:14:58 +1300

    fisher function

--- R/fisher.R
+++ R/fisher.R
@@ -1,5 +1,5 @@
-fisher = function(df, sos = NA, w_size = 8, w_incre = 1) {
-  if (is.na(sos)) {
+fisher = function(df, sos = c(), w_size = 8, w_incre = 1) {
+  if (length(sos) == 0) {
     sos = sost(df)
   }
   FI_final = c()
@@ -44,11 +44,31 @@ fisher = function(df, sos = NA, w_size = 8, w_incre = 1) {
           }
         }
         prob = c(0, lengths(Bin_1) / length(Bin_2), 0)
-        print(prob)
-        return()
+        prob_q = sqrt(prob)
+        FI_temp = 0
+        for (i in 1:(length(prob_q) - 1)) {
+          FI_temp = FI_temp + (prob_q[i] - prob_q[i + 1]) ** 2
+        }
+        FI_temp = 4 * FI_temp
+        FI = c(FI, FI_temp)
+
       }
+
+      k_init = c(k_init, which(FI != 8)[1])
+
+      FI_final = rbind(FI_final, FI)
     }
   }
+  if (length(k_init) == 0) {
+    k_init = c(1)
+  }
+  FI_means = rowMeans(FI_final[,min(k_init):ncol(FI_final)])
+  time_windows = df[1:nrow(FI_final) * w_incre + w_size - 1, 1]
+  FI_final = cbind(FI_final, FI_means, time_windows)
+  df_FI = as.data.frame(FI_final)
+  write.table(df_FI, "FI.csv", sep=",", col.names = FALSE, row.names = FALSE)
+  df_FI
+
 }
 
-fisher(df, sos = sos)
+df_FI = fisher(df, sos = sos)
