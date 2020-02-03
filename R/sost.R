040609973b9af96ca09866bc5aecaafab4346560
QuinnAsena <qase352@aucklanduni.ac.nz>
2020-02-03 18:00:59 +1300

    Written some vignette, a description (incomplete), a couple of notes in the code and added axis lables

--- R/sost.R
+++ R/sost.R
@@ -1,3 +1,8 @@
+# The sost function calculates the size of state following:
+# Cabezas, H., Campbell, D., Eason, T., Garmestani, A. S., Heberling, M. T., Hopton, M. E., Templeton, J., White, D., Zanowick, M., & Sparks, R. T. (2010). San Luis Basin sustainability metrics project: A methodology for evaluating regional sustainability. USEPA. USA.
+# Eason, T., & Cabezas, H. (2012). Evaluating the sustainability of a regional system using Fisher information in the San Luis Basin, Colorado. Journal of Environmental Management, 94(1), 41–49. https://doi.org/10.1016/j.jenvman.2011.08.003
+# Ahmad, N., Derrible, S., Eason, T., & Cabezas, H. (2016). Using Fisher information to track stability in multivariate systems. Royal Society Open Science, 3(11), 160582. https://doi.org/10.1098/rsos.160582
+
 sost = function(df, w_size = 8) {
   sos = c()
   df[is.na(df)] = 0
